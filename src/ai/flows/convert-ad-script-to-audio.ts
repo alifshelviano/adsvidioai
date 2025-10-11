b97a4fe6410ac163d7358714eac31e4cc58e6489
Sleepy Emu <noisychickk@gmail.com>
2025-10-11 03:33:20 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 'wa

--- src/ai/flows/convert-ad-script-to-audio.ts
+++ src/ai/flows/convert-ad-script-to-audio.ts
@@ -38,7 +38,7 @@ async function toWav(
       bitDepth: sampleWidth * 8,
     });
 
-    let bufs = [] as any[];
+    const bufs: Buffer[] = [];
     writer.on('error', reject);
     writer.on('data', function (d) {
       bufs.push(d);
